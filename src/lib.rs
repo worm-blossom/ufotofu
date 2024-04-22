3114f9ec8a18f334fd85298dfacdbc21f1878bf7
glyph <glyph@mycelial.technology>
2024-04-22 14:21:34 +0100

    elide lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use core::mem::MaybeUninit;
 //pub mod nb;
 pub mod sync;
 
-pub(crate) fn maybe_uninit_slice_mut<'a, T>(s: &'a mut [T]) -> &'a mut [MaybeUninit<T>] {
+pub(crate) fn maybe_uninit_slice_mut<T>(s: &mut [T]) -> &mut [MaybeUninit<T>] {
     let ptr = s.as_mut_ptr().cast::<MaybeUninit<T>>();
     unsafe { core::slice::from_raw_parts_mut(ptr, s.len()) }
 }
