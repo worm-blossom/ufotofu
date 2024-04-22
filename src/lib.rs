313078097561905caba7788482001554ddee50a8
glyph <glyph@mycelial.technology>
2024-04-22 12:21:40 +0100

    add maybe_uninit_slice_mut

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 #![feature(maybe_uninit_write_slice)]
 
+use core::mem::MaybeUninit;
+
 //pub mod nb;
 pub mod sync;
+
+pub(crate) fn maybe_uninit_slice_mut<'a, T>(s: &'a mut [T]) -> &'a mut [MaybeUninit<T>] {
+    let ptr = s.as_mut_ptr().cast::<MaybeUninit<T>>();
+    unsafe { core::slice::from_raw_parts_mut(ptr, s.len()) }
+}
