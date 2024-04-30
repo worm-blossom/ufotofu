cb7fe4a4803664e9bd6f8a11998baf523432a52d
glyph <glyph@mycelial.technology>
2024-04-30 11:50:18 +0100

    fix the generic typing for Cursor

--- src/sync/consumer/cursor.rs
+++ src/sync/consumer/cursor.rs
@@ -17,11 +17,11 @@ impl From<!> for CursorFullError {
 }
 
 /// Consumes data into a mutable slice.
-pub struct Cursor<I>(Invariant<I>);
+pub struct Cursor<'a, T>(Invariant<CursorInner<'a, T>>);
 
 /// Creates a consumer which places consumed data into the given slice.
-impl<I> Cursor<I> {
-    pub fn new<T>(slice: &mut [T]) -> Cursor<CursorInner<'_, T>> {
+impl<'a, T> Cursor<'a, T> {
+    pub fn new(slice: &mut [T]) -> Cursor<'_, T> {
         // Wrap the inner cursor in the invariant type.
         let invariant = Invariant::new(CursorInner(slice, 0));
 
@@ -29,48 +29,35 @@ impl<I> Cursor<I> {
     }
 }
 
-impl<I, T> AsRef<[T]> for Cursor<I>
-where
-    I: AsRef<[T]>,
-{
+impl<'a, T> AsRef<[T]> for Cursor<'a, T> {
     fn as_ref(&self) -> &[T] {
         let inner = self.0.as_ref();
         inner.as_ref()
     }
 }
 
-impl<I, T> AsMut<[T]> for Cursor<I>
-where
-    I: AsMut<[T]>,
-{
+impl<'a, T> AsMut<[T]> for Cursor<'a, T> {
     fn as_mut(&mut self) -> &mut [T] {
         let inner = self.0.as_mut();
         inner.as_mut()
     }
 }
 
-impl<'a, I, T> Wrapper<&'a [T]> for Cursor<I>
-where
-    I: Wrapper<&'a [T]>,
-{
+impl<'a, T> Wrapper<&'a [T]> for Cursor<'a, T> {
     fn into_inner(self) -> &'a [T] {
         let inner = self.0.into_inner();
         inner.into_inner()
     }
 }
 
-impl<I, T, F, E> Consumer for Cursor<I>
-where
-    I: BulkConsumer<Item = T, Final = F, Error = E>,
-    T: Copy,
-{
+impl<'a, T: Copy> Consumer for Cursor<'a, T> {
     /// The type of the items to be consumed.
     type Item = T;
     /// The value signifying the end of the consumed sequence.
-    type Final = F;
+    type Final = ();
     /// The value emitted when the consumer is full and a subsequent
     /// call is made to `consume()` or `consumer_slots()`.
-    type Error = E;
+    type Error = CursorFullError;
 
     fn consume(&mut self, item: T) -> Result<(), Self::Error> {
         self.0.consume(item)
@@ -81,22 +68,13 @@ where
     }
 }
 
-impl<I, T, F, E> BufferedConsumer for Cursor<I>
-where
-    I: BufferedConsumer<Item = T, Final = F, Error = E>
-        + BulkConsumer<Item = T, Final = F, Error = E>,
-    T: Copy,
-{
+impl<'a, T: Copy> BufferedConsumer for Cursor<'a, T> {
     fn flush(&mut self) -> Result<(), Self::Error> {
         self.0.flush()
     }
 }
 
-impl<I, T, F, E> BulkConsumer for Cursor<I>
-where
-    I: BulkConsumer<Item = T, Final = F, Error = E>,
-    T: Copy,
-{
+impl<'a, T: Copy> BulkConsumer for Cursor<'a, T> {
     fn consumer_slots(&mut self) -> Result<&mut [MaybeUninit<Self::Item>], Self::Error> {
         self.0.consumer_slots()
     }
