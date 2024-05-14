9a3904025e471de2c2e09777b262e84e007cd5c8
glyph <glyph@mycelial.technology>
2024-05-14 11:47:19 +0100

    add first-pass implementation of Scramble producer fuzzer

---
+++ fuzz/fuzz_targets/scramble_producer.rs
@@ -0,0 +1,70 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{arbitrary, arbitrary::Arbitrary};
+
+use core::cmp::min;
+
+use wrapper::Wrapper;
+
+use ufotofu::sync::consumer::Cursor as ConsumerCursor;
+use ufotofu::sync::producer::Cursor as ProducerCursor;
+use ufotofu::sync::producer::{ProduceOperations, Scramble, ScrambleError};
+use ufotofu::sync::{self, BufferedConsumer};
+
+fn data_is_invalid(data: TestData) -> bool {
+    if data.consumer_buf.len() < data.producer_buf.len() {
+        return true;
+    }
+
+    if data.capacity_a < 1 || data.capacity_a > 2048 {
+        return true;
+    }
+
+    if data.capacity_b < 1 || data.capacity_b > 2048 {
+        return true;
+    }
+
+    false
+}
+
+#[derive(Debug, Clone, Arbitrary)]
+struct TestData {
+    consumer_buf: Box<[u8]>,
+    producer_buf: Box<[u8]>,
+    operations_a: ProduceOperations,
+    operations_b: ProduceOperations,
+    capacity_a: usize,
+    capacity_b: usize,
+}
+
+fuzz_target!(|data: TestData| {
+    if data_is_invalid(data.clone()) {
+        return;
+    }
+
+    let TestData {
+        mut producer_buf,
+        mut consumer_buf,
+        operations_a,
+        operations_b,
+        capacity_a,
+        capacity_b,
+    } = data;
+
+    let cursor = ProducerCursor::new(&mut producer_buf[..]);
+
+    let mut i = ConsumerCursor::new(&mut consumer_buf[..]);
+    let mut o = Scramble::new(
+        Scramble::new(cursor, operations_b, capacity_b),
+        operations_a,
+        capacity_a,
+    );
+
+    let _ = sync::bulk_pipe::<_, _, ScrambleError>(&mut o, &mut i);
+    let _ = i.flush();
+
+    // Compare the producer and consumer cursors.
+    let o = o.into_inner().into_inner();
+    let m = min(o.as_ref().len(), i.as_ref().len());
+    assert_eq!(&i.as_ref()[..m], &o.as_ref()[..m]);
+});
