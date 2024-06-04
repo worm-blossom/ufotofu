ba11f33de51525435a2dcf473e06dd96a40b55a7
glyph <glyph@mycelial.technology>
2024-06-04 11:10:23 +0100

    add local_nb fuzzers and smol dependency

---
+++ fuzz/fuzz_targets/local_nb/into_vec_fallible.rs
@@ -0,0 +1,35 @@
+#![no_main]
+#![feature(never_type)]
+
+use libfuzzer_sys::fuzz_target;
+
+use ufotofu::local_nb;
+use ufotofu::local_nb::consumer::IntoVecFallible;
+use ufotofu::local_nb::producer::Cursor as ProducerCursor;
+
+fn fuzz_pipe(data: &[u8]) {
+    smol::block_on(async {
+        let mut o = ProducerCursor::new(&data[..]);
+        let mut i = IntoVecFallible::new();
+
+        let _ = local_nb::pipe(&mut o, &mut i).await;
+
+        assert_eq!(&i.into_vec(), &data[..]);
+    })
+}
+
+fn fuzz_bulk_pipe(data: &[u8]) {
+    smol::block_on(async {
+        let mut o = ProducerCursor::new(&data[..]);
+        let mut i = IntoVecFallible::new();
+
+        let _ = local_nb::bulk_pipe(&mut o, &mut i).await;
+
+        assert_eq!(&i.into_vec(), &data[..]);
+    })
+}
+
+fuzz_target!(|data: &[u8]| {
+    fuzz_pipe(data);
+    fuzz_bulk_pipe(data);
+});
