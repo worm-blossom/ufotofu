5f29177d1c4b89f5688aac97b12e1d2632bc0491
glyph <glyph@mycelial.technology>
2024-06-12 13:48:52 +0100

    conditionally compile the IntoVec types

--- src/local_nb/consumer.rs
+++ src/local_nb/consumer.rs
@@ -1,5 +1,8 @@
+#[cfg(any(feature = "std", feature = "alloc"))]
 mod into_vec;
+#[cfg(any(feature = "std", feature = "alloc"))]
 mod into_vec_fallible;
+
 mod invariant;
 mod invariant_noop;
 mod slice_consumer;
@@ -7,8 +10,11 @@ mod slice_consumer;
 #[cfg(feature = "dev")]
 mod scramble;
 
+#[cfg(any(feature = "std", feature = "alloc"))]
 pub use into_vec::IntoVec;
+#[cfg(any(feature = "std", feature = "alloc"))]
 pub use into_vec_fallible::IntoVecFallible;
+
 pub use slice_consumer::SliceConsumer;
 
 #[cfg(feature = "dev")]
