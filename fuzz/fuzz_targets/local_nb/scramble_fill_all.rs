19be8fb4cfd65a55203950b4106f1200568d648f
glyph <glyph@mycelial.technology>
2024-06-25 09:48:24 +0100

    use variable-capacity output buffers

--- fuzz/fuzz_targets/local_nb/scramble_fill_all.rs
+++ fuzz/fuzz_targets/local_nb/scramble_fill_all.rs
@@ -2,30 +2,41 @@
 #![feature(never_type)]
 #![feature(maybe_uninit_uninit_array, maybe_uninit_slice)]
 
-use libfuzzer_sys::fuzz_target;
-use libfuzzer_sys::{arbitrary, arbitrary::Arbitrary};
-
 use core::cmp::min;
 use core::mem::MaybeUninit;
 
-use wrapper::Wrapper;
+use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{arbitrary, arbitrary::Arbitrary};
 
 use ufotofu::local_nb;
 use ufotofu::local_nb::producer::{ProduceOperations, Scramble, SliceProducer};
-
-fn data_is_invalid(data: &TestData) -> bool {
-    if data.capacity < 1 || data.capacity > 2048 {
-        return true;
-    }
-
-    false
-}
 
 #[derive(Debug, Clone, Arbitrary)]
 struct TestData {
     producer_buffer: Box<[u8]>,
     operations: ProduceOperations,
     capacity: usize,
+    output_buffer_capacity: usize,
+}
+
+fn data_is_invalid(data: &TestData) -> bool {
+    if data.capacity < 1 || data.capacity > 2048 {
+        return true;
+    }
+
+    if data.output_buffer_capacity < 1 || data.output_buffer_capacity > 2048 {
+        return true;
+    }
+
+    false
+}
+
+// This allows us to create a `MaybeUninit` boxed slice using a capacity
+// value provided by the fuzzer.
+fn create_uninit_boxed_slice(slice_capacity: usize) -> Box<[MaybeUninit<u8>]> {
+    std::iter::repeat_with(MaybeUninit::uninit)
+        .take(slice_capacity)
+        .collect()
 }
 
 fuzz_target!(|data: TestData| {
@@ -38,25 +49,26 @@ fuzz_target!(|data: TestData| {
             mut producer_buffer,
             operations,
             capacity,
+            output_buffer_capacity,
         } = data;
 
         // Producer.
         let slice_producer = SliceProducer::new(&mut producer_buffer[..]);
 
         // Scrambler wrapping an inner `slice_producer`.
-        let mut o = Scramble::new(slice_producer, operations, capacity);
+        let mut scrambled_producer = Scramble::new(slice_producer, operations, capacity);
 
         // Buffer to fill with items from the producer.
-        let mut buffer: [MaybeUninit<u8>; 2048] = MaybeUninit::uninit_array();
+        let mut output_buffer = create_uninit_boxed_slice(output_buffer_capacity);
 
         // Fill the buffer with items from the producer.
-        let (items, _maybe_uninit_) = local_nb::fill_all(&mut buffer, &mut o).await.unwrap();
+        let (items, _maybe_uninit) =
+            local_nb::fill_all(&mut output_buffer, &mut scrambled_producer)
+                .await
+                .unwrap();
 
-        // Access the inner producer (`slice_producer`).
-        let o = o.into_inner();
-
-        // Compare the contents of the items buffer and producer.
-        let m = min(items.len(), o.as_ref().len());
-        assert_eq!(&items[..m], &o.as_ref()[..m]);
+        // Compare the contents of the producer buffer and items buffer.
+        let m = min(producer_buffer.len(), items.len());
+        assert_eq!(&producer_buffer[..m], &items[..m]);
     })
 });
