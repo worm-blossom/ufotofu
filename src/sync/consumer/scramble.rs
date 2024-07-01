9e11d6e0dbcd7104c6b7579eb9bee38964801a26
Aljoscha Meyer <mail@aljoscha-meyer.de>
2024-07-01 16:24:36 +0200

    Add TestConsumer

--- src/sync/consumer/scramble.rs
+++ src/sync/consumer/scramble.rs
@@ -9,7 +9,7 @@ use alloc::boxed::Box;
 use std::boxed::Box;
 
 use arbitrary::{Arbitrary, Error as ArbitraryError, Unstructured};
-use ufotofu_queues::fixed::Fixed;
+use ufotofu_queues::Fixed;
 use ufotofu_queues::Queue;
 use wrapper::Wrapper;
 
