37a8cae7f12a5cfd003ed28ab1ee0af811d8fbd6
Sam Gwilym <sam@gwil.garden>
2024-07-04 15:25:56 +0100

    here ya go

--- fuzz/fuzz_targets/sync/pipe_from_slice.rs
+++ fuzz/fuzz_targets/sync/pipe_from_slice.rs
@@ -5,29 +5,29 @@ use libfuzzer_sys::fuzz_target;
 use ufotofu::sync::consumer::{pipe_from_slice, TestConsumer};
 
 fuzz_target!(|data: (TestConsumer<u16, u8, u8>, Vec<u16>)| {
-    let (mut producer, mut items) = data;
+    let (mut consumer, mut items) = data;
 
-    println!("{:?}", producer.as_ref());
-
-    match pipe_from_slice(&mut items, &mut producer) {
+    match pipe_from_slice(&mut items, &mut consumer) {
         Ok(_) => {
             // Were the bytes successfully piped?
 
             // refactor as assert_eq
-            if producer.as_ref() != items.as_slice() {
+            if consumer.as_ref() != items.as_slice() {
+                println!("{:?}", consumer.as_ref());
+                println!("{:?}", items.as_slice());
                 panic!("Bytes piped into slice did not match!")
             }
         }
         Err(err) => {
-            // let producer_data = producer.peek_slice();
+            // let consumer_data = consumer.peek_slice();
 
             // If the filled property is not equal to the inner slice, panic.
             // assertion macro
-            if *producer.as_ref() == items[0..err.consumed] {
-                panic!("Consumed was not the same as the producer slice.")
+            if *consumer.as_ref() == items[0..err.consumed] {
+                panic!("Consumed was not the same as the consumer slice.")
             }
 
-            // Do we need to check if the error produced was the right kind? e.g. by inspecting the producer's termination field?
+            // Do we need to check if the error produced was the right kind? e.g. by inspecting the consumer's termination field?
         }
     }
 });
